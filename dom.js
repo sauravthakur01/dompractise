8fa99281530cff8ab6325f2044e32f8569f4c3e2
Sanjay Pal <sanjaypal001@gmail.com>
2022-09-01 16:27:49 +0530

    task-6

--- dom.js
+++ dom.js
@@ -12,6 +12,8 @@
 // const list = document.getElementsByClassName("list-group-item");
 // list[0].style.backgroundColor = "grey";
 
+
+
 // let add = document.getElementsByClassName("title");
 // add[0].style.color ="green";
 // add[0].style.fontWeight="bold"
@@ -19,16 +21,31 @@
 //     a.style.backgroundColor ="grey";
 // }
 
+
+
 // let list =document.getElementsByClassName("list-group-item");
 // list[2].style.backgroundColor = "green";
-
 // for(let li of list){
 //     li.style.fontWeight ="bold";
 // }
 
-let li =document.getElementsByTagName("li");
-li[2].style.backgroundColor = "green";
 
-for(let lis of li){
-    lis.style.fontWeight ="bold";
+
+// let li =document.getElementsByTagName("li");
+// li[2].style.backgroundColor = "green";
+// for(let lis of li){
+//     lis.style.fontWeight ="bold";
+// }
+
+
+// var item = document.querySelector('.list-group-item:nth-child(2)');
+// item.style.backgroundColor = "green";
+// document.querySelector('.list-group-item:nth-child(3)').style.display = "none";
+
+var item = document.querySelectorAll('.list-group-item');
+item[1].style.color = "green";
+
+var odd = document.querySelectorAll('.list-group-item:nth-child(odd)');
+for(let i = 0 ; i <odd.length ;i++){
+odd[i].style.backgroundColor="green";
 }
